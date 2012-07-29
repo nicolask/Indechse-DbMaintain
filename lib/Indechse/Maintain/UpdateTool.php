9e0ac70ad387d458094f3fdde46d2fa05024d920
Nicolas Krueger <nicolas.krueger@gmail.com>
2012-07-29 15:19:13 +0200

    added the ability to write seperate update files for different dbms (postgres and mysql at the moment)

--- lib/Indechse/Maintain/UpdateTool.php
+++ lib/Indechse/Maintain/UpdateTool.php
@@ -45,24 +45,29 @@ class Indechse_Maintain_UpdateTool
             $currentRevision = $this->_getCurrentRevision();
             // loop through all of the files
             while (false !== ($fname = readdir($dhandle))) {
-                if (($fname != '.') && ($fname != '..')) {
-                    if (!is_dir($dir . "/" . $fname)) {
-                        if (preg_match("/.*\.(php|sql)$/", $fname)) {
-                            $number = intval(strstr($fname, '.', true));
-                            if ($number == 0) {
-                                throw new Exception("the update file '{$fname}' does not contain the revision number");
-                            }
-                            if ($number <= $currentRevision) continue;
-                            if (!isset($this->_availableUpdates[$number])) {
-                                $this->_availableUpdates[$number] = array();
-                                
-                            }
-                            $this->_availableUpdates[$number][] = $fname;
+                if (preg_match("/^\./", $fname)) {
+                    continue;
+                }
+                if (!is_dir($dir . "/" . $fname)) {
+                    if (preg_match("/^(\d*)\.((pgsql|mysql)\.)?(.*)\.(php|sql)$/", $fname, $matches)) {
+                        if (count($matches) != 6) {
+                            printf("invalid filename '%s', skipped file ", $fname);
+                            continue;
                         }
-                            
-                    } else {
-                        $this->_scanUpdatesFolder($dir . "/" . $fname, $parts);
+                        $number = intval($matches[1]);
+                        if ($number == 0) {
+                            printf("the update file '%s' does not contain a revision number in first place", $fname);
+                        }
+                        if ($number <= $currentRevision) continue;
+                        if (!isset($this->_availableUpdates[$number])) {
+                            $this->_availableUpdates[$number] = array();
+
+                        }
+                        $this->_availableUpdates[$number][] = $matches;
                     }
+
+                } else {
+                    $this->_scanUpdatesFolder($dir . "/" . $fname, $parts);
                 }
             }
             ksort($this->_availableUpdates);
@@ -76,26 +81,28 @@ class Indechse_Maintain_UpdateTool
     private function _performUpdates() {
         foreach($this->_availableUpdates as $rev => $updateList) {
             foreach($updateList as $u) {
-                preg_match("/\d*\.([A-Za-z0-9_]*)\.(php|sql)/", $u, $matches);
-                $className = 'Update_'.$matches[1];
-                $updateFormat = $matches[2];
+                if (!empty($u[3]) && $this->_conn->getAttribute(PDO::ATTR_DRIVER_NAME) != $u[3]) {
+                    continue;
+                }
+                $updateFormat = $u[5];
                 $this->_conn->beginTransaction();
                 try {
                     $instance = null;
                     if ($updateFormat == 'sql') {
                         require_once('Indechse/Maintain/Update/SqlExecute.php');
                         $instance = new Indechse_Maintain_Update_SqlExecute($this->_conn, $rev);
-                        $instance->setSql(file_get_contents($this->_updateLocation.'/'.$u));
+                        $instance->setSql(file_get_contents($this->_updateLocation.'/'.$u[0]));
                     } else {
-                        require_once($this->_updateLocation.'/'.$u);
+                        $className = 'Update_'.$u[4];
+                        require_once($this->_updateLocation.'/'.$u[0]);
                         $instance = new $className($this->_conn, $rev);
                     }
                     $instance->update();
-                    $this->_markUpdateComplete($rev, $className);
+                    $this->_markUpdateComplete($rev, $u[4].'.'.$u[5]);
                     $this->_conn->commit();
                 } catch (Exception $ex) {
                     $this->_conn->rollBack();
-                    throw new Exception(sprintf("Update %s (%d) failed with message: %s", $className, $rev, $ex->getMessage()), $ex->getCode(), $ex);
+                    throw new Exception(sprintf("Update %s (%d) failed with message: %s", $u[0], $rev, $ex->getMessage()), $ex->getCode(), $ex);
                 }
             }
         }
@@ -110,7 +117,7 @@ class Indechse_Maintain_UpdateTool
     private function _markUpdateComplete($rev, $classname) {
         $stmt = $this->_conn->prepare("INSERT INTO dbrev (revision, updatename) VALUES (?,?)");
         $stmt->execute(array($rev, $classname));
-        printf("%s (%d) completed", $classname, $rev);
+        printf('#%2$d %1$s () completed'."\n", $classname, $rev);
         
     }
     
